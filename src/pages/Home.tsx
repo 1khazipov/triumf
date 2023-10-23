e79c86e1cfe461f563922374c32f03c0f6a369ec
1khazipov <aaooo00aa0@gmail.com>
2023-10-23 19:26:11 +0300

    initital commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home page!</h1>
+        </div>
+    );
+};
+
+export default Home;
