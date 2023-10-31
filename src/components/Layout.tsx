4737214660dd4873e431775f48d028c017a271e6
LeylaKhb <leyla.khbi@gmail.com>
2023-10-31 17:13:10 +0300

    Added tabbed schedule

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from 'react';
 import head_img from '../static/head_img.png';
+import instagram from '../static/instagram.png';
+import vk from '../static/vk.png';
 import { Link } from 'react-router-dom';
 import EmblemHeader from './EmblemHeader';
 import EmblemFooter from './EmblemFooter';
@@ -25,28 +27,36 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                         <EmblemHeader />
                     </Link>
                     <div className='text-white'>
-                        <h1 style={{ textAlign: 'center', font:'inter', color: '#ffffff', fontSize: '1.5rem', transform: 'translateY(25%) translateX(-25%)' }}>
+                        <h1 style={{ textAlign: 'center', font:'inter', color: '#ffffff', fontSize: '1.5vw', transform: 'translateY(25%) translateX(-25%)' }}>
                             ГИЛЬДИЯ РЕПЕТИТОРОВ
                         </h1>
-                        <h1 style={{ textAlign: 'center', font:'inter', color: '#ffffff', fontSize: '3rem', transform: 'translateY(-25%) translateX(-25%)' }}>
+                        <h1 style={{ textAlign: 'center', font:'inter', color: '#ffffff', fontSize: '3vw', transform: 'translateY(-25%) translateX(-25%)' }}>
                             ТРИУМФ
                         </h1>
                     </div>
 
-                    <div className='info-about' style={{ position: 'absolute', right: 50, top: 0}}>
-                        <p style={{ font:'inter', color: '#ffffff', fontSize: '0.8rem' }}>
+                    <div className='info-about' style={{ position: 'absolute', right: 50, top: 0, display: 'flex', flexFlow: 'column', color: '#ffffff', font:'inter' }}>
+                        <div style={{ fontSize: '0.8vw', marginTop: 10 }}>
                             Запись по телефонам:
-                        </p>
-                        <p style={{ textAlign: 'center', font:'inter', fontSize: '1.0rem' }}>
+                        </div>
+                        <div style={{ fontSize: '1.0vw', marginTop: 8}}>
                             <a href="tel:+78432589759" style={{color: '#ffffff'}}>+7 (843) 258-97-59, </a>
-                            <a href="tel:+79600489759" style={{color: '#ffffff'}}>+7 960 048-97-59</a>
-                        </p>
-                        <p style={{ font:'inter', color: '#ffffff', fontSize: '0.8rem' }}>
+                            <a href="tel:+79600489759" style={{color: '#ffffff'}}>+7 (960) 048-97-59</a>
+                        </div>
+                        <div style={{ fontSize: '0.8vw', marginTop: 8 }}>
                             Адрес:
-                        </p>
-                        <p style={{ textAlign: 'center', font:'inter', color: '#ffffff', fontSize: '1.0rem' }}>
+                        </div>
+                        <div style={{fontSize: '1.0vw', marginTop: 8 }}>
                             <a href="https://yandex.ru/maps/-/CDaDiQnM" style={{color: '#ffffff'}}>г. Казань, ул. Жуковского, д. 8, офис 313</a>
-                        </p>
+                        </div>
+                        <div style={{ marginTop: 9, display: 'flex'}}>
+                            <a href="https://vk.com/kazantriumf" >
+                                <img src={vk} alt="vk" style={{ height: 30, width: 30, marginTop: 0 }} />
+                            </a>
+                            <a href="https://www.instagram.com/_triumf_/" >
+                                <img src={instagram}  alt="instagram" style={{ height: 30, width: 30, marginTop: 0, marginLeft: 8 }} />
+                            </a>
+                        </div>
                     </div>
 
                     <div className="button-row" style={{ position: 'absolute', display: 'flex',
@@ -84,20 +94,21 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             </main>
 
             <footer className='w-full' style={{ position: 'relative' }}>
-                <div className="footer-container" style={{ backgroundColor: '#002e5d', height: '200px', width: '100%', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                <div className="footer-container" style={{ backgroundColor: '#002e5d', height: '200px', backgroundSize: 'cover', backgroundPosition: 'center', flexFlow: 'row' }}>
                     <div className="footer-column" style={{ display: 'flex', flexFlow: 'column', paddingTop: 5}}>
                         <Link to="/">
                             <EmblemFooter />
                         </Link>
 
-                        <h1 style={{ textAlign: 'center', width: '10%', height: '10%', color: '#ffffff', fontSize: '0.65rem', transform: 'translateY(45%) translateX(15%)' }}>
+                        <h1 style={{ textAlign: 'center', width: '10%', height: '10%', color: '#ffffff', fontSize: '0.65vw', transform: 'translateY(45%) translateX(15%)' }}>
                             ГИЛЬДИЯ РЕПЕТИТОРОВ
                         </h1>
-                        
-                        <h1 style={{ textAlign: 'center', width: '10%', height: '10%', color: '#ffffff', fontSize: '1.8rem', transform: 'translateY(-35%) translateX(15%)' }}>
+
+                        <h1 style={{ textAlign: 'center', width: '10%', height: '10%', color: '#ffffff', fontSize: '1.8vw', transform: 'translateY(-35%) translateX(15%)' }}>
                             ТРИУМФ
                         </h1>
                     </div>
+
                 </div>
             </footer>
         </div>
